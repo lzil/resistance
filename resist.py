6157d0cad958d40a0ead05ed88821d7f8a78e13a
Jingyu Li <Mousie@dhcp-18-111-10-63.dyn.mit.edu>
2015-09-02 21:39:00 -0400

    hola

--- resist.py
+++ resist.py
@@ -1,3 +1,5 @@
 import os, sys
 
-numPlayers = input("Please enter the number of players: ")
+numPlayers = input("Please enter the number of players (5-10): ")
+
+missionNumbers = {5: [2, 3, 2, 3, 3], 6: [2, 3, 4, 3, 4], 7: [2, 3, 3, 4, 4], 8: [3, 4, 4, 5, 5], 9: [3, 4, 4, 5, 5], 10: [3, 4, 4, 5, 5]}
